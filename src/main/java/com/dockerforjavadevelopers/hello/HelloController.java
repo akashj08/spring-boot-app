974008dc962d1035644793c655589d3c655317eb
Akash Jaiswal <ajaiswal@noon.com>
2021-02-11 16:33:49 +0530

    test

--- src/main/java/com/dockerforjavadevelopers/hello/HelloController.java
+++ src/main/java/com/dockerforjavadevelopers/hello/HelloController.java
@@ -9,7 +9,7 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "Hello World vedansh\n";
+        return "Hello World Akash\n";
     }
     
 }
